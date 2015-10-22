f4a2e6421323de0ffccf7ea3ade7e81c325725a9
Anil <anilgeorge55@gmail.com>
2015-10-22 11:00:33 -0500

    commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
+## Creating a script that creates two functions that computes inverse of matrix and also caches the inverse if already computed.
 ## functions do
 
-## Write a short comment describing this function
+## Function to create Special vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y)
+  {
+    x<<-y
+    m<<-NULL
+  }
+  
+  get<-function() x
+  setinverse<-function(inverse) m<<-mean
+  getinverse<-function() m
+  list(set=set,get=get,getinverse=getinverse,setinverse=setinverse)
+  
+  
 }
 
 
-## Write a short comment describing this function
+
+## Function to return inverse of matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  m<-x$getinverse()
+  
+  if(!is.null(m))
+  {
+    message("from cache")
+    return(m)
+  }
+  
+  
+  
+  data<-x$get()
+  m<-solve(data)
+  x$setinverse(m)
+  m
+  
 }
+
+
+
+
